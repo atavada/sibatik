33bbabb3cba9990f75812e973c19d468cf4582c6
atavada <ardhaardhanaputra@gmail.com>
2024-03-08 07:40:39 +0700

    feat: add 'is_read' column and 'user_id' foreign key constraint to messages table

--- database/migrations/2024_03_01_122621_create_messages_table.php
+++ database/migrations/2024_03_01_122621_create_messages_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('email');
             $table->text('message');
+            $table->boolean('is_read')->default(false);
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
