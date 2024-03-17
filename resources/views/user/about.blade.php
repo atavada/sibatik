528ee2aa707618f72724ac39907bfe3972b6b38a
atavada <ardhaardhanaputra@gmail.com>
2024-03-17 21:44:23 +0700

    feat: add AboutController and update about.blade.php

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -112,101 +112,27 @@
 	</div>
 	<!-- end featured section -->
 
-<section class="shop-banner">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <h3>Temukan Tenun Mulya di Instagram!</h3>
-                <p>Sosial media tidak hanya sebagai perantara untuk menghubungkan yang jauh. 
-                    Tidak hanya sekedar menjadi pemberitaan media luar, media sosial memiliki peran krusial dalam melakukan pemasaran. 
-                    Tenun Mulya ikut ambil alih dalam pergerakan digitalisisasi ini. 
-                    Dengan ada nya akun media sosial dapat menjadi media dalam memperluas pasar produk tanah air!
-                </p>
-                <p>Mari bersama-sama kita cintai produk dalam negeri dan dukung UMKM lokal!
-                    Bersama, kita bisa membangun negeri yang lebih kuat dan sejahtera!
-                    #CintaProdukDalamNegeri #DukungUMKMLokal #BanggaBuatanIndonesia</p>
-                <a href="https://www.instagram.com/tenunmulya/" class="cart-btn btn-lg">Instagram</a>
-            </div>
-            <div style="margin-left: 200px;">
-                <img src="frontend/img/mobile.png" alt="Image Description" class="img-fluid" width="240" height="210" style="margin-right: 100px;" >
-            </div>
-        </div>
-    </div>
-</section>
-
-	
-
-	<!-- team section -->
-	<div class="mt-150">
+	<section class="shop-banner">
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-8 offset-lg-2 text-center">
-					<div class="section-title">
-						<h3>Team <span class="orange-text">Develop</span></h3>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, fuga quas itaque eveniet beatae optio.</p>
-					</div>
+				<div class="col-md-6">
+					<h3>Temukan Tenun Mulya di Instagram!</h3>
+					<p>Sosial media tidak hanya sebagai perantara untuk menghubungkan yang jauh. 
+						Tidak hanya sekedar menjadi pemberitaan media luar, media sosial memiliki peran krusial dalam melakukan pemasaran. 
+						Tenun Mulya ikut ambil alih dalam pergerakan digitalisisasi ini. 
+						Dengan ada nya akun media sosial dapat menjadi media dalam memperluas pasar produk tanah air!
+					</p>
+					<p>Mari bersama-sama kita cintai produk dalam negeri dan dukung UMKM lokal!
+						Bersama, kita bisa membangun negeri yang lebih kuat dan sejahtera!
+						#CintaProdukDalamNegeri #DukungUMKMLokal #BanggaBuatanIndonesia</p>
+					<a href="https://www.instagram.com/tenunmulya/" class="cart-btn btn-lg">Instagram</a>
 				</div>
-			</div>
-			<div class="row">
-				<div class="col-lg-4 col-md-6">
-					<div class="single-team-item">
-						<div class="team-bg team-bg-1"></div>
-						<h4>Jimmy Doe <span>Farmer</span></h4>
-						<ul class="social-link-team">
-							<li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-							<li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
-							<li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6">
-					<div class="single-team-item">
-						<div class="team-bg team-bg-2"></div>
-						<h4>Marry Doe <span>Farmer</span></h4>
-						<ul class="social-link-team">
-							<li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-							<li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
-							<li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
-						</ul>
-					</div>
-				</div>
-                <div class="col-lg-4 col-md-6">
-					<div class="single-team-item">
-						<div class="team-bg team-bg-2"></div>
-						<h4>Marry Doe <span>Farmer</span></h4>
-						<ul class="social-link-team">
-							<li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-							<li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
-							<li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0">
-					<div class="single-team-item">
-						<div class="team-bg team-bg-3"></div>
-						<h4>Simon Joe <span>Farmer</span></h4>
-						<ul class="social-link-team">
-							<li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-							<li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
-							<li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
-						</ul>
-					</div>
-				</div>
-                <div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0">
-					<div class="single-team-item">
-						<div class="team-bg team-bg-3"></div>
-						<h4>Simon Joe <span>Farmer</span></h4>
-						<ul class="social-link-team">
-							<li><a href="#" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-							<li><a href="#" target="_blank"><i class="fab fa-twitter"></i></a></li>
-							<li><a href="#" target="_blank"><i class="fab fa-instagram"></i></a></li>
-						</ul>
-					</div>
+				<div style="margin-left: 200px;">
+					<img src="frontend/img/mobile.png" alt="Image Description" class="img-fluid" width="240" height="210" style="margin-right: 100px;" >
 				</div>
 			</div>
 		</div>
-	</div>
-	<!-- end team section -->
+	</section>
 
 	<!-- testimonail-section -->
 	<div class="testimonail-section mt-80 mb-150">
@@ -262,34 +188,4 @@
 		</div>
 	</div>
 	<!-- end testimonail-section -->
-
-	<!-- logo carousel -->
-	<div class="logo-carousel-section">
-		<div class="container">
-			<div class="row">
-				<div class="col-lg-12">
-					<div class="logo-carousel-inner">
-						<div class="single-logo-item">
-							<img src="assets/img/company-logos/1.png" alt="">
-						</div>
-						<div class="single-logo-item">
-							<img src="assets/img/company-logos/2.png" alt="">
-						</div>
-						<div class="single-logo-item">
-							<img src="assets/img/company-logos/3.png" alt="">
-						</div>
-						<div class="single-logo-item">
-							<img src="assets/img/company-logos/4.png" alt="">
-						</div>
-						<div class="single-logo-item">
-							<img src="assets/img/company-logos/5.png" alt="">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- end logo carousel -->
-
-
 @endsection
